f0743aff1793559189f65f75ec9bcaff3ab97139
Darren Cauthon <darren@cauthon.com>
2013-07-19 11:34:15 -0500

    No need for module declaration.

--- lib/starting_blocks/blinky.rb
+++ lib/starting_blocks/blinky.rb
@@ -2,8 +2,4 @@ require 'starting_blocks'
 require 'blinky'
 require "starting_blocks/blinky/version"
 
-module StartingBlocks
-  module Blinky
-    StartingBlocks::Publisher.subscribers << StartingBlocks::Extensions::BlinkyLighting.new
-  end
-end
+StartingBlocks::Publisher.subscribers << StartingBlocks::Extensions::BlinkyLighting.new
--- lib/starting_blocks/blinky/version.rb
+++ lib/starting_blocks/blinky/version.rb
@@ -1,5 +1,5 @@
 module StartingBlocks
   module Blinky
-    VERSION = "0.0.2"
+    VERSION = "0.0.3"
   end
 end
