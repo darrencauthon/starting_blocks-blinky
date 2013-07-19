9efe082f0e0cf073d45b6c1e93bb785895bcc5cd
Darren Cauthon <darren@cauthon.com>
2013-07-19 11:25:15 -0500

    Set the blinky extension, bump the version.

--- lib/starting_blocks/blinky.rb
+++ lib/starting_blocks/blinky.rb
@@ -4,25 +4,6 @@ require "starting_blocks/blinky/version"
 
 module StartingBlocks
   module Blinky
+    StartingBlocks::Publisher.subscribers << StartingBlocks::Extensions::BlinkyLighting.new
   end
-end
-
-class GreenOnSuccessRedOnFailure
-  def receive_results results
-    if (results[:errors] || 0) > 0
-      Blinky.new.light.failure!
-    elsif (results[:failures] || 0) > 0
-      Blinky.new.light.failure!
-    elsif (results[:skips] || 0) > 0
-      Blinky.new.light.building!
-    else
-      Blinky.new.light.success!
-    end
-  end
-end
-StartingBlocks::Publisher.subscribers << [GreenOnSuccessRedOnFailure.new]
-
-at_exit do
-  sleep(0.5)
-  Blinky.new.light.off!
 end
